4ce0a361cf1b66a81d6c30df5d75f368c2c10636
Aryan Khokhar <aryankhokhar022@gmail.com>
2025-09-27 09:33:13 +0530

    virtual Function

--- OOPS/FunOverriding.cpp
+++ OOPS/FunOverriding.cpp
@@ -7,17 +7,28 @@ public:
     void  show(){
         cout<<"parent class show";
     }
+
+    virtual void hello(){
+        cout<<"parent hello \n";
+    }
 };
 class Child :public Parent{
 public:
     void  show(){
         cout<<"Child class show";
     }
+
+    void hello(){
+        cout<<"child hello";
+    }
 };
 
 
 int main(){
     Child c1;
+    Parent *ptr;
+    ptr = &c1;//Run time binding possible due to inheritance;
+    ptr->hello(); //virtual function;
     c1.show();
     return 0;
 }
