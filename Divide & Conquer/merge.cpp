06388ad498b08431e7268accaf8ed2eb144d6c23
Aryan Khokhar <aryankhokhar022@gmail.com>
2025-10-07 23:17:43 +0530

    Divide & Conquer

---
+++ Divide & Conquer/merge.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+void merge(int arr[],int si,int mid,int ei){
+    vector<int> temp;
+    int i=si;
+    int j =mid+1;
+    while(i<=mid && j<=ei){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while(i<=mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j<=ei){
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    //vector -->orgiginal
+
+    for(int idx=si,x=0;idx<=ei;idx++){
+        arr[idx]=temp[x++];
+    }
+}
+
+
+void mergeSort(int arr[],int si,int ei){
+    if(si>=ei){
+        return;
+    }
+    int mid = si+(ei-si)/2;  
+    mergeSort(arr,si,mid); //left half
+    mergeSort(arr,mid+1,ei);//right half
+
+    merge(arr,si,mid,ei);
+}
+
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[6]={6,3,7,5,2,4};
+    int n= 6;
+    mergeSort(arr ,0,n-1);
+    print(arr,n);
+    return 0;
+}
