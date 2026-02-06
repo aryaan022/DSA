09780dc232bbb491c980a3af96d921a09f863727
Aryan Khokhar <aryankhokhar022@gmail.com>
2026-02-06 09:38:23 +0530

    Time: 0 ms (100%), Space: 14.5 MB (8.38%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int n = logs.size();
+        int c=0;
+        stack<string>s;
+        for(int i=0;i<n;i++){
+            if(logs[i]=="../"){
+                if(!s.empty()){
+                    s.pop();
+                    c--;
+                }
+            }
+            else if(logs[i]=="./"){
+                //do nothing
+            }
+            else {
+                s.push(logs[i]);
+                c++;
+            }
+        }
+        return c;
+        
+    }
+};
