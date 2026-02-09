4987c7ac00877f2bcd76941d11f76e8fbe7ff2df
Aryan Khokhar <aryankhokhar022@gmail.com>
2026-02-09 22:32:15 +0530

    Time: 10 ms (54.21%), Space: 23.9 MB (57.63%) - LeetHub

---
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        k =k- n;
+        string res(n , 'a');
+        for(int i = n - 1; i >= 0; --i){
+            int add = min(k , 25);
+            res[i] =res[i]+ add;
+            k =k- add;
+        }
+        return res;
+    }
+};
