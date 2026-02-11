31a265c65de7ee97536c9e3336adccf9d3004fe9
Aryan Khokhar <aryankhokhar022@gmail.com>
2026-02-11 15:13:50 +0530

    Time: 0 ms (100%), Space: 52.2 MB (92.72%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxr =0;
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxr){
+                return false;
+            }
+            maxr=max(maxr,i+nums[i]); 
+        }
+
+        return true;
+    }
+};
