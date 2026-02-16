a90133361a01d7b3f1833ece881f6089a0f42a30
Aryan Khokhar <aryankhokhar022@gmail.com>
2026-02-16 12:09:13 +0530

    Time: 71 ms (46.71%), Space: 82.3 MB (54.52%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
+    
+    void helper(int idx, int k,vector<int>&current,int n)
+    {
+        if(current.size()==k)    // base case
+        {
+            ans.push_back(current);
+            return;
+        }
+        
+        for(int i=idx;i<n+1;i++)
+        {
+            current.push_back(i);  //consider the current element i
+            helper(i+1,k,current,n); // generate combinations
+            current.pop_back(); //proceed to next element
+        }
+    }
+    
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> res;
-        vector<int> comb;
-        function<void(int)> backtrack = [&](int start) {
-            if (comb.size() == k) {
-                res.push_back(comb);
-                return;
-            }
-            for (int i = start; i <= n; ++i) {
-                comb.push_back(i);
-                backtrack(i + 1);
-                comb.pop_back();
-            }
-        };
-        backtrack(1);
-        return res;
+        vector<int>current;
+        helper(1,k,current,n);
+        return ans; //return answer
     }
 };
