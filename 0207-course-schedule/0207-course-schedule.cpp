437d49b62c956a584629059eda91eab08b9b465a
Aryan <aryankhokhar022@gmail.com>
2026-05-21 22:11:57 +0530

    Time: 57 ms (5.01%), Space: 17.7 MB (98.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,15 +3,18 @@ public:
     bool cycle(int src,vector<vector<int>>&graph,vector<bool>&vis,vector<bool>&recpath){
         vis[src]=true;
         recpath[src]=true;
-
-        for(int v:graph[src]){
-            if(!vis[v]){
-                if(cycle(v,graph,vis,recpath)){
+        for(int i=0;i<graph.size();i++){
+            int u= graph[i][1];
+            int v=graph[i][0];
+            if(u==src){
+                if(!vis[v]){
+                    if(cycle(v,graph,vis,recpath)){
+                        return true;
+                    }
+                }
+                else if(recpath[v]==true){
                     return true;
                 }
-            }
-            else if(recpath[v]==true){
-                return true;
             }
         }
         recpath[src]=false;
@@ -19,16 +22,8 @@ public:
     }
 
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
-        vector<vector<int>>graph(numCourses);
-
-        for(auto p:prerequisites){
-            int x=p[0];
-            int y=p[1];
-            graph[y].push_back(x);
-        }
-
+    bool canFinish(int numCourses, vector<vector<int>>& graph) {
+        
         vector<bool> vis(numCourses,false); 
         vector<bool> recpath(numCourses,false); 
 
