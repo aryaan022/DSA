8b378d0a38c693dda4085a8c97f998826a704b1c
Aryan <aryankhokhar022@gmail.com>
2026-06-02 03:17:16 +0530

    Time: 72 ms (92.38%), Space: 44.2 MB (88.71%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,16 +1,15 @@
 class Solution {
-public: 
-    void dij(int src,vector<int>&dist,vector<vector<pair<int,int>>>&graph){
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; //min heap wt[v] , v
+public:
+    void dij(int src,int V,vector<int>&dist,vector<vector<pair<int,int>>>&graph){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; //wt[v],v
         dist[src]=0;
-        pq.push({0,src});
+        pq.push(make_pair(0,src));
         while(!pq.empty()){
             int u=pq.top().second;
             pq.pop();
             for(auto e:graph[u]){
-                int wt= e.first;
-                int v = e.second;
-
+                int v=e.first;
+                int wt=e.second;
                 if(dist[v]>dist[u]+wt){
                     dist[v]=dist[u]+wt;
                     pq.push({dist[v],v});
@@ -19,18 +18,17 @@ public:
         }
     }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        //step 1 build gaph and push value for u , v and wt
+        vector<vector<pair<int,int>>>graph(n+1);
         vector<int>dist(n+1,INT_MAX);
         int ans=INT_MIN;
-        vector<vector<pair<int,int>>>graph(n+1);
-
         for(int i=0;i<times.size();i++){
             int u=times[i][0];
             int v=times[i][1];
             int wt=times[i][2];
-            graph[u].push_back({wt,v});
+            graph[u].push_back(make_pair(v,wt));
         }
-        dij(k,dist,graph);
-
+        dij(k,n,dist,graph);
         for(int i=1;i<=n;i++){
             if(dist[i]==INT_MAX){
                 return -1;
