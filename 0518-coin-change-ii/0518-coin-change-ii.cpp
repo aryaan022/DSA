34fd5adff2658feda3011d713866e45eba9d48b6
Aryan <aryankhokhar022@gmail.com>
2026-06-05 22:05:50 +0530

    Time: 27 ms (64.44%), Space: 37.2 MB (23.35%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int rec(int n,int amount ,vector<int>&coins,vector<vector<int>>&dp){
+        if(n==0){
+            return 0;
+        }
+        if(amount == 0){
+            return 1;
+        }
+        if(dp[n][amount]!=-1){
+            return dp[n][amount];
+        }
+        if(coins[n-1]<=amount){
+            return dp[n][amount]= rec(n,amount-coins[n-1],coins,dp)+rec(n-1,amount,coins,dp); 
+        }
+        return dp[n][amount]= rec(n-1,amount,coins,dp);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        return rec(coins.size(),amount,coins,dp);
+    }
+};
