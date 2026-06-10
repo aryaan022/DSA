c41b398ff8bf7311ca81d170f584e58b505b51e6
Aryan <aryankhokhar022@gmail.com>
2026-06-10 19:51:59 +0530

    Time: 55 ms (17.95%), Space: 54.1 MB (6.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,32 @@
 class Solution {
-public:
+public: 
     const int INF=INT_MAX-1;
-    int recur(int n ,vector<int>&coins,int amount,vector<vector<int>>&dp){
+
+    int rec(int n,vector<int>&coins , vector<vector<int>>&dp,int amount){
         if(amount==0){
             return 0;
         }
-        if(n == 0){
+        if(n==0){
             return INF;
         }
         if(dp[n][amount]!=-1){
             return dp[n][amount];
         }
-
         if(coins[n-1]<=amount){
-            return dp[n][amount] =min(1+recur(n,coins,amount-coins[n-1],dp),recur(n-1,coins,amount,dp));
+            return dp[n][amount]=min(1+rec(n,coins,dp,amount-coins[n-1]),rec(n-1,coins,dp,amount));
         }
-        return dp[n][amount]=recur(n-1,coins,amount,dp);
+        else{
+            return dp[n][amount]=rec(n-1,coins,dp,amount);
+        }
+
+        return dp[n][amount];
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-        int ans =recur(coins.size(),coins,amount,dp);
+        int ans = rec(coins.size(),coins,dp,amount);
         if(ans==INF){
             return -1;
         }
         return ans;
     }
-
 };
