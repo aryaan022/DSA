f82be16daa9a3e0bff26cd33ea26c82fa0fe7787
Aryan <aryankhokhar022@gmail.com>
2026-06-12 18:22:12 +0530

    Time: 65 ms (23.32%), Space: 15.8 MB (51.97%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    bool isMatch(string t, string p) {
-        int n= t.size();
-        int m = p.size();
-
+    bool isMatch(string s, string p) {
+        int n=s.size();
+        int m =p.size();
         vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
-
         dp[0][0]=true;
-        for(int j=1;j<=m;j++){ // initalization of 0th Row --> tex is "" (empty);
+
+        for(int j=1;j<=m;j++){ //initilization for the smallest problem when row =0  it menas string khalii hai and patter hai ;
             if(p[j-1]=='*'){
                 dp[0][j]=dp[0][j-1];
             }
-            else{//"?" ya english character will always be false;
+            else{
                 dp[0][j]=false;
             }
         }
 
-        for(int i=1;i<=n; i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(t[i-1]==p[j-1] || p[j-1]=='?'){
+                if(p[j-1]==s[i-1]||p[j-1]=='?'){
                     dp[i][j]=dp[i-1][j-1];
-                }else if(p[j-1]=='*'){
+                }
+                else if(p[j-1]=='*'){
                     dp[i][j]=dp[i-1][j] || dp[i][j-1];
-                }else{
+                }
+                else{
                     dp[i][j]=false;
                 }
+
             }
         }
         return dp[n][m];
-
     }
 };
