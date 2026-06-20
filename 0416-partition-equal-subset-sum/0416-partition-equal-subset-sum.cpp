e2640c2f8cea01e18a80e2d81c9b6fb699a60e5c
Aryan <aryankhokhar022@gmail.com>
2026-06-20 23:08:13 +0530

    Time: 39 ms (94.88%), Space: 111.2 MB (19.53%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    bool recur(int n,vector<int>&nums,int t,vector<vector<int>>&dp){
-        if(t==0){
+    bool solve(int n, int W , vector<int>&nums,vector<vector<int>>&dp){
+        if(W==0){
             return true;
         }
         if(n==0){
             return false;
         }
-        if(dp[n][t]!=-1){
-            return dp[n][t];
+        if(dp[n][W]!=-1){
+            return dp[n][W];
         }
-        if(nums[n-1]<=t){
-            return dp[n][t]= recur(n-1,nums,t-nums[n-1],dp) || recur(n-1,nums,t,dp);
+        if(nums[n-1]<=W)//valid
+        {
+            return dp[n][W]=solve(n-1,W-nums[n-1],nums,dp) || solve(n-1,W,nums,dp);
         }
-        return dp[n][t]=recur(n-1,nums,t,dp);
+        return dp[n][W]=solve(n-1,W,nums,dp);
     }
     bool canPartition(vector<int>& nums) {
         int ts=0;
-        for(int i=0;i<nums.size();i++){
+        for(int i =0;i<nums.size();i++){
             ts=ts+nums[i];
         }
         if(ts%2!=0){
             return false;
         }
-        vector<vector<int>>dp(nums.size()+1,vector<int>((ts/2)+1,-1));
-        return recur(nums.size(),nums,ts/2,dp);
+        int W=ts/2;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(W+1,-1));
+        return solve(nums.size(),W,nums,dp);
     }
 };
