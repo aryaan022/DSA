5f77a2f3af813c9e1e143ef47ec19a06be5335e8
Aryan <aryankhokhar022@gmail.com>
2026-06-20 22:45:51 +0530

    Time: 68 ms (5.04%), Space: 17.5 MB (98.02%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    bool isCycle(int src,vector<bool>&vis,vector<bool>&recpath,vector<vector<int>>&graph){
+    bool cycle(int src, vector<bool>&recpath,vector<bool>&vis,vector<vector<int>>&graph){
         vis[src]=true;
         recpath[src]=true;
         for(int i=0;i<graph.size();i++){
-            int u=graph[i][1];
-            int v=graph[i][0];
+            int u = graph[i][1];
+            int v = graph[i][0];
             if(u==src){
                 if(!vis[v]){
-                    if(isCycle(v,vis,recpath,graph)){
-                        return  true;
+                    if(cycle(v,recpath,vis,graph)){
+                        return true;
                     }
                 }
-                else if(recpath[v]==true){
+                else if(recpath[v]){
                     return true;
                 }
             }
@@ -21,51 +21,41 @@ public:
         return false;
     }
 
-    void topoSort(int src,vector<bool>&vis,stack<int>&s,vector<vector<int>>& graph){
+    void toposort(int src,vector<bool>&vis,stack<int>&s,vector<vector<int>>&graph){
         vis[src]=true;
         for(int i=0;i<graph.size();i++){
-            int u=graph[i][1];
-            int v=graph[i][0];
+            int u =graph[i][1];
+            int v= graph[i][0];
             if(u==src){
                 if(!vis[v]){
-                    topoSort(v,vis,s,graph);
+                    toposort(v,vis,s,graph);
                 }
-                
             }
-            
         }
         s.push(src);
     }
 
-    // void calcuIndeg(vector<int>&indeg,vector<vector<int>>& graph){
-    //     for(int i=0;i<graph.size();i++){
-    //         int u=graph[i][1];
-    //         int v=graph[i][0];
 
-    //         indeg[v]++;
-    //     }
-        
-    // }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& graph) {
-        
-        int v=numCourses;
-        vector<bool>vis(v,false);
-        vector<bool>recpath(v,false);
-        vector<int> ans;
-        for(int i=0;i<v;i++){
+    vector<int>findOrder(int numCourses, vector<vector<int>>&graph) {
+        vector<bool>recpath(numCourses,false);
+        vector<bool>vis(numCourses,false);
+        vector<int>ans;
+
+        for(int i =0;i<numCourses;i++){
             if(!vis[i]){
-                if(isCycle(i,vis,recpath,graph)){
+                if(cycle(i,recpath,vis,graph)){
                     return ans;
                 }
             }
         }
 
-        //topo sorting
-        stack<int>s; 
-        vector<bool>vis2(v,false);
-        for(int i=0;i<v;i++){
+
+        //performing topo sort now using DFS
+        stack<int>s;
+        vector<bool>vis2(numCourses,false);
+        for(int i=0;i<numCourses;i++){
             if(!vis2[i]){
-                topoSort(i,vis2,s,graph);
+                toposort(i,vis2,s,graph);
             }
         }
 
@@ -74,45 +64,7 @@ public:
             s.pop();
         }
         return ans;
-
-
-
-        //using kan's algorithim logic
-        // int V=numCourses;
-        // vector<vector<int>>adj(V); 
-        // vector<int>ans;
-        // vector<int> indeg(V,0);
-        // queue<int>q;
         
-        // calcuIndeg(indeg,graph);
-
-        // for(auto p:graph){
-        //     int u = p[1];
-        //     int v = p[0];
-        //     adj[u].push_back(v);
-        // }
-
-        // for(int i=0;i<V;i++){
-        //     if(indeg[i]==0){
-        //         q.push(i);
-        //     }
-        // }
-        // while(!q.empty()){
-        //     int curr=q.front();
-        //     q.pop();
-        //     ans.push_back(curr);
-        //     for(int ne:adj[curr]){
-        //         indeg[ne]--;
-        //         if(indeg[ne]==0){
-        //             q.push(ne);
-        //         }
-        //     }
-        // }
-
-        // if(ans.size() != V){
-        //     return {};
-        // }
-        // return ans;
 
     }
 };
