53d423545e73292f86a4ca7051248cd8d0dccc8d
Aryan <aryankhokhar022@gmail.com>
2026-06-23 23:43:42 +0530

    Time: 26 ms (63.08%), Space: 16.3 MB (79.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,41 @@
 class Solution {
-public: 
+public:
+    void solve(int r,int c,vector<vector<char>>& grid,int n,int m){
+        grid[r][c]=0;
 
-    void dfs(int r, int c,vector<vector<char>>&graph,int n,int m){
-        graph[r][c]='0';
         //up
-        if(r-1>=0 && graph[r-1][c]=='1'){
-            dfs(r-1,c,graph,n,m);
+        if(r-1>=0&&grid[r-1][c]=='1'){
+            solve(r-1,c,grid,n,m);
         }
+
         //down
-        if(r+1<n && graph[r+1][c]=='1'){
-            dfs(r+1,c,graph,n,m);
+        if(r+1<n&&grid[r+1][c]=='1'){
+            solve(r+1,c,grid,n,m);
         }
-        //left
-        if(c-1>=0 && graph[r][c-1]=='1'){
-            dfs(r,c-1,graph,n,m);
-        }
+
         //right
-        if(c+1<m && graph[r][c+1]=='1'){
-            dfs(r,c+1,graph,n,m);
+        if(c+1<m &&grid[r][c+1]=='1'){
+            solve(r,c+1,grid,n,m);
+        }
+
+        //left
+        if(c-1>=0 && grid[r][c-1]=='1'){
+            solve(r,c-1,grid,n,m);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-      int n= grid.size();
-      int m = grid[0].size();
-      int count =0;
-      for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j]=='1'){
-                dfs(i,j,grid,n,m);
-                count = count+1;
+        int n= grid.size();
+        int m = grid[0].size();
+        int c=0;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    solve(i,j,grid,n,m);
+                    c +=1;
+                }
             }
+
         }
-      }
-      return count;  
+        return c;
     }
 };
