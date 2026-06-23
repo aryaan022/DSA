65481ecc03e4893aafbd3e8883445244b21bb57b
Aryan <aryankhokhar022@gmail.com>
2026-06-24 00:09:10 +0530

    Time: 4 ms (81.72%), Space: 13.3 MB (28.77%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
+        int n =word1.size();
+        int m =word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-
-        for(int j=0;j<=m;j++){
-            dp[0][j]=j;
-        }
         for(int i=0;i<=n;i++){
             dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
         }
 
 
@@ -19,11 +18,10 @@ public:
                     dp[i][j]=0+dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+                    dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j-1],dp[i-1][j]));
                 }
             }
         }
         return dp[n][m];
-
     }
 };
