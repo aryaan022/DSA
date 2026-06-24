d3e04e0b6e1cc2f929d187cc4c87bdb2eb723253
Aryan <aryankhokhar022@gmail.com>
2026-06-24 21:47:05 +0530

    Time: 60 ms (84.35%), Space: 148.6 MB (63.61%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,33 +1,44 @@
 class MedianFinder {
 public:
-    priority_queue<int> pq_left; // max heap
-    priority_queue<int, vector<int>, greater<>> pq_right; // min heap
 
-    MedianFinder() {}
+    priority_queue<int>pq; //max heap
+    priority_queue<int  ,vector<int> ,greater<>>pq_right; // min heap
+    MedianFinder() {
+        
+    }
 
+    
     void addNum(int num) {
-        // insert in correct heap
-        if (pq_left.empty() || num <= pq_left.top()) {
-            pq_left.push(num);
-        } else {
+        if(pq.empty() || num<=pq.top()){
+            pq.push(num);
+        }
+        else{
             pq_right.push(num);
         }
+        
 
-        // balance sizes
-        if (pq_left.size() > pq_right.size() + 1) {
-            pq_right.push(pq_left.top());
-            pq_left.pop();
+        //balancing both the side
+        if(pq.size()>pq_right.size()+1){
+            pq_right.push(pq.top());
+            pq.pop();
         }
-        else if (pq_left.size() < pq_right.size()) {
-            pq_left.push(pq_right.top());
+        else if(pq.size()<pq_right.size()){
+            pq.push(pq_right.top());
             pq_right.pop();
         }
     }
-
+    
     double findMedian() {
-        if (pq_left.size() == pq_right.size()) {
-            return (pq_left.top() + pq_right.top()) / 2.0;
+        if(pq.size()==pq_right.size()){
+            return (pq.top()+pq_right.top()) / 2.0;
         }
-        return pq_left.top();
+        return pq.top();
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
