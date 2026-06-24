933d5e34340a6c002b8f963911965809f557cd73
Aryan <aryankhokhar022@gmail.com>
2026-06-24 22:13:15 +0530

    Time: 3 ms (61.27%), Space: 10.4 MB (41.06%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -3,21 +3,31 @@ public:
     vector<vector<string>> result;
 
     bool isSafe(vector<string>& board, int row, int col, int n) {
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') 
-            return false;
-        }
 
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q')
-             return false;
+        //horiontal
+        for(int j=0;j<n;j++){
+            if(board[row][j]=='Q'){
+                return false;
+            }
         }
-
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q') 
-            return false;
+        //vertical
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
         }
-
+        //left diagonal 
+        for(int i=row,j=col; i>=0&&j>=0; i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        //right diagnol
+        for(int i=row,j=col; i>=0&&j<n; i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
         return true;
     }
 
@@ -26,7 +36,7 @@ public:
             result.push_back(board);
             return;
         }
-
+        
         for (int col = 0; col < n; col++) {
             if (isSafe(board, row, col, n)) {
                 board[row][col] = 'Q';
