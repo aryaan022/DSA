7b5dab87fcc1fe06dc9ccd353fe2381d50cd13bf
Aryan <aryankhokhar022@gmail.com>
2026-07-08 04:21:53 +0530

    Time: 56 ms (5.04%), Space: 17.6 MB (99.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    bool cycle(int src, vector<bool>&recpath,vector<bool>&vis,vector<vector<int>>& graph){
-        vis[src]=true;
+    bool isCycle(int src,vector<bool>&recpath,vector<bool>&isvis , vector<vector<int>>& graph){
         recpath[src]=true;
+        isvis[src]=true;
+
         for(int i=0;i<graph.size();i++){
             int u = graph[i][1];
             int v = graph[i][0];
+
             if(u==src){
-                if(!vis[v]){
-                    if(cycle(v,recpath,vis,graph)){
+                if(!isvis[v]){
+                    if(isCycle(v,recpath,isvis,graph)){
                         return true;
                     }
                 }
                 else if(recpath[v]){
                     return true;
                 }
+
             }
         }
         recpath[src]=false;
         return false;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& graph) {
 
-        vector<bool>vis(numCourses,false);
+    bool canFinish(int numCourses, vector<vector<int>>& graph) {
         vector<bool>recpath(numCourses,false);
+        vector<bool>isvis(numCourses,false);
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(cycle(i,recpath,vis,graph)){
+            if(!isvis[i]){
+                if(isCycle(i,recpath,isvis,graph)){
                     return false;
                 }
             }
