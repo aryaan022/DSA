dbcbc278cfc1be7df48016cbacfb23176a111bba
Aryan <aryankhokhar022@gmail.com>
2026-07-24 01:30:16 +0530

    Time: 0 ms (100%), Space: 14 MB (93.9%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void bt(int n ,vector<int>& candidates, int target,vector<int>&curr,vector<vector<int>>&ans){
-
-        if(n==candidates.size()){
-            if(target ==0){
+    void cal(int n,vector<int>&arr,int t,vector<int>&curr,vector<vector<int>>&ans){
+        if(n==arr.size()){
+            if(t==0){
                 ans.push_back(curr);
             }
             return;
         }
+        
 
-
-        if(candidates[n]<=target){
-            curr.push_back(candidates[n]);
-            bt(n,candidates,target-candidates[n],curr,ans);
+        if(arr[n]<=t){
+            //taaking the number
+            curr.push_back(arr[n]);
+            cal(n,arr,t-arr[n],curr,ans);
             curr.pop_back();
         }
-
-        bt(n+1,candidates,target,curr,ans);
+        cal(n+1,arr,t,curr,ans);
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& arr, int t) {
         vector<int>curr;
         vector<vector<int>>ans;
-        bt(0,candidates,target,curr,ans);
+        cal(0,arr,t,curr,ans);
         return ans;
     }
 };
