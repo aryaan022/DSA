88af08077c2329cf56437be20bffb1ebb40955d3
Aryan <aryankhokhar022@gmail.com>
2026-08-05 23:42:57 +0530

    Time: 0 ms (100%), Space: 16.9 MB (51.6%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,59 +1,56 @@
 class Solution {
 public:
-    void bfs(int n,int m,vector<vector<int>>& grid,int &count){
+    void bfs(int m ,int n,vector<vector<int>>& g,int &c){
         queue<pair<int,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(g[i][j]==2){
                     q.push(make_pair(i,j));
                 }
             }
         }
-
         while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                int row=q.front().first;
-                int col=q.front().second;
+            int s=q.size();
+            for(int i =0;i<s;i++){
+                int r=q.front().first;
+                int c=q.front().second;
                 q.pop();
 
-                //dowm
-                if(row+1<n && grid[row+1][col]==1){
-                    grid[row+1][col]=2;
-                    q.push(make_pair(row+1,col));
-                }
-
                 //up
-                if(row-1>=0 && grid[row-1][col]==1){
-                    grid[row-1][col]=2;
-                    q.push(make_pair(row-1,col));
+                if(r-1>=0 &&g[r-1][c]==1){
+                    g[r-1][c]=2;
+                    q.push({r-1,c});
+                }
+                //down
+                if(r+1<m && g[r+1][c]==1){
+                    g[r+1][c]=2;
+                    q.push({r+1,c});
+                }
+                //right
+                if(c+1<n && g[r][c+1]==1){
+                    g[r][c+1]=2;
+                    q.push({r,c+1});
                 }
 
                 //left
-                if(col-1>=0 && grid[row][col-1]==1){
-                    grid[row][col-1]=2;
-                    q.push(make_pair(row,col-1));
-                }
-
-                //right
-                if(col+1<m && grid[row][col+1]==1){
-                    grid[row][col+1]=2;
-                    q.push(make_pair(row,col+1));
+                if(c-1>=0 && g[r][c-1]==1){
+                    g[r][c-1]=2;
+                    q.push({r,c-1});
                 }
             }
-            if(!q.empty()){   
-                count++;
+            if(!q.empty()){
+                c++;
             }
         }
     }
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int count =0;
-        bfs(n,m,grid,count);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
+    int orangesRotting(vector<vector<int>>& g) {
+        int  count=0;
+        int m =g.size();
+        int n = g[0].size();
+        bfs(m,n,g,count);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(g[i][j]==1){
                     return -1;
                 }
             }
